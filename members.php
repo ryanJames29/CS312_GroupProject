cf7c3cc1a91f26ff0dd79d41ab5e41a2e97c8806
“Ryan <ryanj29@colostate.edu>
2024-04-01 12:46:36 -0600

    added info/general layout for members

--- members.php
+++ members.php
@@ -11,4 +11,17 @@
 <body>
     <?php include 'navbar.php'; ?>  
     <h1>Members</h1>
+
+    <h2> Ryan James </h2>
+    <img src="" alt="" width="" height="">
+    <p> I'm a senior computer science major at Colorado State University with a general concentration. After school, I am interested in
+        getting into web development or software engineering. I also have an interest in cyber security. I enjoy being outdoors, watching sports,
+        and being around friends and family.
+    </p>
+
+    <h2> Group Member 2 </h2>
+
+    <h2> Group Member 3 </h2>
+
+    <h2> Group Member 4 </h2>
 </body> 
