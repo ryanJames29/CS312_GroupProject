740441492fa1524b84d9e1322e9dd04e9b36c823
“Ryan <ryanj29@colostate.edu>
2024-04-01 13:14:00 -0600

    navbar update

--- navbar.php
+++ navbar.php
@@ -1,13 +1,4 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="author" content="">
-  <meta name="description" content="CS312 Group Project">
-  <meta name="keywords" content="">
-  <title>CS312</title>
-</head>
-
 <body>
     <div class="navbar">
         <a href="https://cs.colostate.edu:4444/~ryanj29/CS312_GroupProject/index.php">Home</a>
