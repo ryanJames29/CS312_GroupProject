d828151a6c47fd8ed70f62bec762c2115133ebde
“Ryan <ryanj29@colostate.edu>
2024-04-01 12:17:32 -0600

    index.php Initial File

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="author" content="">
+  <meta name="description" content="CS312 Group Project">
+  <meta name="keywords" content="">
+  <title>CS312</title>
+</head>
+
+<body>
+    <h1>Title Here</h1>
+</body> 
