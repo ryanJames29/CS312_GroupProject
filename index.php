f73d27a66ae03a63963cfcb857d2ef0a2ce81a44
rm0819 <rm0819@colostate.edu>
2024-04-06 08:46:15 -0600

    Put the table generator in its own page

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
   <meta name="keywords" content="">
   <title>Team 18 LLC</title>
   <link rel="stylesheet" href="./style.css">
-  <script src="./js/tableGenerator.js"></script>
   <link rel="icon" type="image/png" sizes="32x32" href="./images/logo.png">
 </head>
 
@@ -15,19 +14,5 @@
     <?php include 'navbar.php'; ?>  
     <h1>Team 18 LLC</h1>
     <img src="./images/logo.png" alt="Team 18 LLC" width="300" height="200">
-
-    <h2>Table Generator</h2>
-
-    <label for="rowColCount">Enter number for Rows & Columns (1-26)</label><br>
-    <input class="input" type="number" id="rowColCount" min="1" max="26">
-    <br><br>
-    <label for="colorCount">Enter number of Colors (1-10)</label><br>
-    <input class="input" type="number" id="colorCount" min="1" max="10">
-    <br><br>
-    <button onclick="generateTables()">Generate Tables</button><br>
-   
-
-    <div id="errorMessage" class="errorMessage"></div>
-    <div id="tablesContainer"></div>  
 
 </body> 
--- navbar.php
+++ navbar.php
@@ -3,6 +3,7 @@
     <div class="navbar">
         <a class="link" href="./index.php">Home</a>
         <a class="link" href="./members.php">Members</a>
+        <a class="link" href="./tablegen.php">Table</a>
     </div>
     <hr class="navbar">
 </body> 
